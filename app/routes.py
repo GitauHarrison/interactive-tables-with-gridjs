7a8d5a27f8dd92e4c80115006d516be7c8a32f33
harry <harrisongitau3@gmail.com>
2022-06-26 15:07:17 +0300

    Add server-side table functionality

--- app/routes.py
+++ app/routes.py
@@ -37,3 +37,59 @@ def ajax_table():
 @app.route('/ajax-table-data')
 def ajax_data():
     return {'data': [user.to_dict() for user in User.query.all()]}
+
+
+@app.route('/server-side-table')
+def server_side_table():
+    users = User.query
+    if users.count() >= 10000:
+        users = users.limit(10000)
+    else:
+        create_fake_users(500)
+        print("All users are created: ", users.count())
+    return render_template(
+        'server_side_table.html',
+        users=users,
+        title='Server-side table')
+
+
+@app.route('/server-side-table-data')
+def server_side_table_data():
+    query = User.query
+
+    # Search filter
+    search = request.args.get('search')
+    if search:
+        query = query.filter(db.or_(
+            User.name.like(f'%{search}%'),
+            User.email.like(f'{search}%')
+        ))
+    total = query.count()
+
+    # Sorting
+    sort = request.args.get('sort')
+    if sort:
+        order = []
+        for s in sort.split(','):
+            direction = s[0]
+            name = s[1:]
+            if name not in ['name', 'email', 'age']:
+                name = 'name'
+            col = getattr(User, name)
+            if direction == '-':
+                col = col.desc()
+            order.append(col)
+        if order:
+            query = query.order_by(*order)
+
+    # Pagination
+    start = request.args.get('start', type=int, default=-1)
+    length = request.args.get('length', type=int, default=-1)
+    if start != -1 and length != -1:
+        query = query.offset(start).limit(length)
+
+    # Response
+    return {
+        'data': [user.to_dict() for user in query.all()],
+        'total': total
+    }
